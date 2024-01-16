9af45756273ca9e2e86f7e44bd9c201700c261e0
MuzGeek <emamuzo.okerri@gmail.com>
2024-01-16 03:23:47 +0100

    minor update

--- src/color.js
+++ src/color.js
@@ -1,9 +1,3 @@
 "use strict";
 
 import { hexToRgb, rgbToHex } from "./converters";
-
-console.log(
-	`Hex to RGB\nHex: #234AC3 - RGB: ${hexToRgb(
-		"#234AC3"
-	)}\nRGB to Hex\nRGB: 35, 74, 195 - Hex: ${rgbToHex([35, 74, 195]).toUpperCase()}`
-);
