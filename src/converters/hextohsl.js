35ae167434f3557d2276b24ba1e36ad1e847c3f0
MuzGeek <emamuzo.okerri@gmail.com>
2024-01-16 02:07:46 +0100

    feature: setup Hex to HSL converter function

---
+++ src/converters/hextohsl.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const convertHextoHsl = function (hex) {};
+
+export default convertHextoHsl;
--- src/converters/index.js
+++ src/converters/index.js
@@ -1,4 +1,5 @@
 "use strict";
 
 export { default as hexToRgb } from "./hextorgb";
+export { default as hexToHsl } from "./hextohsl";
 export { default as rgbToHex } from "./rgbtohex";
