98f31b85c3a60cc66d6a6f1f91052fd820d103ad
MuzGeek <emamuzo.okerri@gmail.com>
2024-01-18 21:31:09 +0100

    update: added JSDoc and ToDo comments

--- src/converters/hsltorgb.js
+++ src/converters/hsltorgb.js
@@ -1,5 +1,16 @@
 "use strict";
 
+/*
+ * TODO:
+ * Allow string as accepted parameter
+ * Allow option to select the ouput format - Array or String
+ */
+
+/**
+ * Converts a color from HSL color space to RGB color space
+ * @param {Array} arr The HSL value of the color to be converted
+ * @returns The samd color in RGB color space
+ */
 const convertHslToRgb = function (arr) {
 	let [h, s, l] = [...arr];
 	let r, g, b, r_, g_, b_;
