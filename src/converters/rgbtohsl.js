c1e7f5ca3bd81c2652e04c28ac2c239641086661
MuzGeek <emamuzo.okerri@gmail.com>
2024-01-20 16:24:07 +0100

    update: added option to choose output format

--- src/converters/rgbtohsl.js
+++ src/converters/rgbtohsl.js
@@ -8,11 +8,12 @@
 
 /**
  * Convert a color from RGB color space to HSL color space
- * @param {Array, String} arr The RGB value of the color to be converted
+ * @param {Array, String} val The RGB value of the color to be converted
+ * @param {String} f The format of the output color
  * @returns The HSL value of the same color
  */
-const convertRgbToHsl = function (val) {
-	let r, g, b;
+const convertRgbToHsl = function (val, f = "array") {
+	let r, g, b, h, s, l;
 
 	if (Array.isArray(val)) [r, g, b] = val.map((v) => v / 255);
 	if (typeof val === "string")
@@ -24,9 +25,7 @@ const convertRgbToHsl = function (val) {
 
 	const minVal = Math.min(r, g, b);
 	const maxVal = Math.max(r, g, b);
-	const l = (minVal + maxVal) / 2;
-
-	let h, s;
+	l = (minVal + maxVal) / 2;
 
 	if (minVal === maxVal) {
 		h = s = 0;
@@ -46,10 +45,14 @@ const convertRgbToHsl = function (val) {
 				h = (r - g) / d + 4;
 				break;
 		}
-		h /= 6;
+
+		h = Math.round((h / 6) * 360);
+		s = Math.round(s * 100);
+		l = Math.round(l * 100);
 	}
 
-	return [Math.round(h * 360), Math.round(s * 100), Math.round(l * 100)];
+	if (f === "string") return `HSL(${h}, ${s}%, ${l}%)`;
+	else return [h, s, l];
 };
 
 export default convertRgbToHsl;
